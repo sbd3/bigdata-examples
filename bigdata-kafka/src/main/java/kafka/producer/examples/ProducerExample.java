d1e47c8ab077d7592763a7d3d1c99556f4ca0da4
Puneet Singh <kadian.puneet@gmail.com>
2017-03-06 17:43:04 +0530

    Modules added to pom

--- bigdata-kafka/src/main/java/kafka/producer/examples/ProducerExample.java
+++ bigdata-kafka/src/main/java/kafka/producer/examples/ProducerExample.java
@@ -10,37 +10,37 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
 public class ProducerExample {
-    
-    private static final String KAFKA_TOPIC = "test-topic";
+
+    private static final String KAFKA_TOPIC = "coco";
 
     private Properties getKafkaProperties() {
-	Properties kafkaProps = new Properties();
-	kafkaProps.put("bootstrap.servers", "10.2.0.61:6667");
-	kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-	kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-	return kafkaProps;
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("bootstrap.servers", "10.5.3.166:6667");
+        kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return kafkaProps;
     }
-    
+
     private KafkaProducer<String, String> getKafkaProducer() {
-	KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(getKafkaProperties());
-	return kafkaProducer;
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(getKafkaProperties());
+        return kafkaProducer;
     }
-    
+
     private ProducerRecord<String, String> getProducerRecord() {
-	ProducerRecord<String, String> record = new ProducerRecord<String, String>(KAFKA_TOPIC, "key1", "value1");
-	return record;
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(KAFKA_TOPIC, "key1", "value1");
+        return record;
     }
-    
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-	ProducerExample ex = new ProducerExample();
-	KafkaProducer<String, String> producer = ex.getKafkaProducer();
-	Future<RecordMetadata> metadata = producer.send(ex.getProducerRecord(), new Callback() {
-	    @Override
-	    public void onCompletion(RecordMetadata metadata, Exception ex) {
-		System.out.println(metadata.offset() + ":" + metadata.partition());
-		System.out.println(ex.getMessage());
-	    }
-	});
-	System.out.println(metadata.get().topic());
+        ProducerExample ex = new ProducerExample();
+        KafkaProducer<String, String> producer = ex.getKafkaProducer();
+        Future<RecordMetadata> metadata = producer.send(ex.getProducerRecord(), new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception ex) {
+                System.out.println(metadata.offset() + ":" + metadata.partition());
+                System.out.println(ex.getMessage());
+            }
+        });
+        System.out.println(metadata.get().topic());
     }
 }
