2cf9282c460540efc69ffb3f39a541e471ee7018
Puneet Singh <kadian.puneet@gmail.com>
2017-04-21 15:36:50 +0530

    Percolation classes stub added.

---
+++ bigdata-java/src/main/java/org/coursera/algorithm/partone/Percolation.java
@@ -0,0 +1,36 @@
+package org.coursera.algorithm.partone;
+
+public class Percolation {
+
+	// create n-by-n grid, with all sites blocked
+	public Percolation(int n) {
+	}
+
+	// open site (row, col) if it is not open already
+	public void open(int row, int col) {
+	}
+
+	// is site (row, col) open?
+	public boolean isOpen(int row, int col) {
+		return false;
+	}
+
+	// is site (row, col) full?
+	public boolean isFull(int row, int col) {
+		return false;
+	}
+
+	// number of open sites
+	public int numberOfOpenSites() {
+		return 0;
+	}
+
+	// does the system percolate?
+	public boolean percolates() {
+		return false;
+	}
+
+	// test client (optional)
+	public static void main(String[] args) {
+	}
+}
---
+++ bigdata-java/src/main/java/org/coursera/algorithm/partone/PercolationStats.java
@@ -0,0 +1,32 @@
+package org.coursera.algorithm.partone;
+
+public class PercolationStats {
+
+	// perform trials independent experiments on an n-by-n grid
+	public PercolationStats(int n, int trials) {
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+		return 0L;
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		return 0L;
+	}
+
+	// low endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return 0L;
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return 0L;
+	}
+
+	// test client
+	public static void main(String[] args) {
+	}
+}
